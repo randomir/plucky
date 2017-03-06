d245bb8a7989b7f96e5d23a670cc0be7f5317092
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2017-03-06 14:12:48 +0100

    added tests for merge method

---
+++ tests/test_merge.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# encoding: utf8
+import os
+import sys
+sys.path.append(os.pardir)
+
+import unittest
+from plucky import merge
+
+
+class TestMerge(unittest.TestCase):
+    def test_leaf_int(self):
+        self.assertEqual(merge(1, 2), 3)
+
+    def test_leaf_int_none(self):
+        self.assertRaises(TypeError, merge, 1, None)
+
+    def test_leaf_float(self):
+        self.assertEqual(merge(1.2, 2.3), 3.5)
+
+    def test_leaf_float_int(self):
+        self.assertEqual(merge(1.2, 3), 4.2)
+
+    def test_leaf_str(self):
+        self.assertEqual(merge("abc", "def"), "abcdef")
+
+    def test_leaf_str_num(self):
+        self.assertRaises(TypeError, merge, "abc", 1)
+
+    def test_dict_with_string_values(self):
+        self.assertEqual(merge({"a": "x"}, {"a": "y"}), {"a": "xy"})
+
+    def test_dict_with_different_keysets(self):
+        self.assertEqual(merge({"a": "x"}, {"b": "y"}), {"a": "x", "b": "y"})
+
+    def test_dict_with_numeric_values(self):
+        self.assertEqual(merge({"a": 1.2}, {"a": 2.3}), {"a": 3.5})
+
+    def test_lists_of_equal_len(self):
+        self.assertEqual(merge([1, 2], [3, 4]), [4, 6])
+
+    def test_lists_of_inequal_len(self):
+        self.assertEqual(merge([1, 2], [3]), [1, 2, 3])
+
+
+if __name__ == '__main__':
+    unittest.main()
