492cc4a8824e2b5c83d6be5424f95d11f1645b80
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2017-05-02 02:57:10 +0200

    bumped to v0.3.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='plucky',
-    version='0.3.6',
+    version='0.3.7',
     description='Plucking (deep) keys/paths safely from python collections has never been easier.',
     long_description=open('README.rst').read(),
     author='Radomir Stevanovic',
